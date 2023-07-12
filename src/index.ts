ded975bfa1258040b2c29b89d910289aec27022b
mailorcanto <mailorcanto@gmail.com>
2023-07-11 21:24:27 -0300

    feat: server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Grêmio');
+import express from 'express';
+
+import {config} from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get('/gremio', (request, response) => response.send('O Grêmio vai sair campeão'));
+
+app.listen(port, () => console.log('Listening on port 3333'));

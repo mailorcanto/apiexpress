10d515f694dab223f86d89a948cc4efb0687f82c
mailorcanto <mailorcanto@gmail.com>
2023-07-14 19:04:19 -0300

    hello

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ const port = process.env.PORT || 3333;
 app.get('/gremio', (request, response) => response.send('O Grêmio vai sair campeão'));
 
 app.listen(port, () => console.log('Listening on port 3333'));
+
